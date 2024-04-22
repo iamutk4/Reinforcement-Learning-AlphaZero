c9d2faf9fb0e0cacb963a6448b05a4d2b26ee9a1
Asankoli <111701203+Asankoli@users.noreply.github.com>
2024-04-22 05:22:53 -0400

    dependencies

--- ConnectFour_UI/backend/app.py
+++ ConnectFour_UI/backend/app.py
@@ -18,7 +18,13 @@ model.eval()
 args = {
     'C': 2,
     'num_searches': 600,
-    'dirichlet_epsilon': 0.,
+    'num_iterations': 8,
+    'num_selfPlay_iterations': 500,
+    'num_parallel_games': 100,
+    'num_epochs': 4,
+    'batch_size': 128,
+    'temperature': 1.25,
+    'dirichlet_epsilon': 0.25,
     'dirichlet_alpha': 0.3
 }
 
